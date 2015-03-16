37a51ed65b00ab930e163aa8ac7daf3a13001a81
zmuzik <zbynek.muzik@gmail.com>
2015-03-16 22:04:20 +0800

    camera pics

--- app/src/main/java/zmuzik/slidingpuzzle/Conf.java
+++ app/src/main/java/zmuzik/slidingpuzzle/Conf.java
@@ -8,6 +8,8 @@ public class Conf {
     public static final int GRID_COLUMNS_LANDSCAPE = 3;
 
     public static final int PIC_QUALITY = 90;
-    public static final String PICTURE_FILE_SUFFIX = "jpg";
+    public static final String PICTURE_FILE_SUFFIX = ".jpg";
     public static final int MAX_PIC_SIZE = 1000;
+    public static final String APP_PIC_SUBDIR = "SlidingPuzzle";
+
 }
