ea7927bfe6da943c1df6751fb18128ada68d19ca
zmuzik <zbynek.muzik@gmail.com>
2015-03-16 11:43:42 +0800

    picasso, assets gridview

--- app/src/main/java/zmuzik/slidingpuzzle/Conf.java
+++ app/src/main/java/zmuzik/slidingpuzzle/Conf.java
@@ -3,4 +3,8 @@ package zmuzik.slidingpuzzle;
 public class Conf {
     public static final int DEFAULT_GRID_DIM_SHORT = 4;
     public static final int DEFAULT_GRID_DIM_LONG = 4;
+
+    public static final int PIC_QUALITY = 90;
+    public static final String PICTURE_FILE_SUFFIX = "jpg";
+    public static final int MAX_PIC_SIZE = 1000;
 }
