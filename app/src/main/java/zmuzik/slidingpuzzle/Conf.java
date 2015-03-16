0fba97a91a2f78f392026c6eceb1a893af7b5d76
zmuzik <zbynek.muzik@gmail.com>
2015-03-16 16:13:13 +0800

    moved params to Conf

--- app/src/main/java/zmuzik/slidingpuzzle/Conf.java
+++ app/src/main/java/zmuzik/slidingpuzzle/Conf.java
@@ -4,6 +4,9 @@ public class Conf {
     public static final int DEFAULT_GRID_DIM_SHORT = 4;
     public static final int DEFAULT_GRID_DIM_LONG = 4;
 
+    public static final int GRID_COLUMNS_PORTRAIT = 2;
+    public static final int GRID_COLUMNS_LANDSCAPE = 3;
+
     public static final int PIC_QUALITY = 90;
     public static final String PICTURE_FILE_SUFFIX = "jpg";
     public static final int MAX_PIC_SIZE = 1000;
