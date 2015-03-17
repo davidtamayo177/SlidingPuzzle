827f5e4c98d398433b632ad2cb074384357269d1
zmuzik <zbynek.muzik@gmail.com>
2015-03-18 00:35:28 +0800

    flickr pivot

--- app/src/main/java/zmuzik/slidingpuzzle/Conf.java
+++ app/src/main/java/zmuzik/slidingpuzzle/Conf.java
@@ -7,9 +7,13 @@ public class Conf {
     public static final int GRID_COLUMNS_PORTRAIT = 2;
     public static final int GRID_COLUMNS_LANDSCAPE = 3;
 
+    public static final String DCIM_APP_SUBDIR = "SlidingPuzzle";
     public static final int PIC_QUALITY = 90;
     public static final String PICTURE_FILE_SUFFIX = ".jpg";
     public static final int MAX_PIC_SIZE = 1000;
-    public static final String APP_PIC_SUBDIR = "SlidingPuzzle";
+
+    public static final String FLICKR_API_ROOT = "https://api.flickr.com/services/rest";
+    public static final String FLICKR_API_KEY = "f1434369cbe71e80f75e43f8d374ca39";
+    public static final String FLICKR_API_SECRET = "d269d44d6a4d1ec4";
 
 }
