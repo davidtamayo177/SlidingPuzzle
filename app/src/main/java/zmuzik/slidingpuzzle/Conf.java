c4c4598fa32897b68573d0df573cb05341612022
zmuzik <zbynek.muzik@gmail.com>
2015-03-25 20:25:06 +0800

    paging, new pictures, etc

--- app/src/main/java/zmuzik/slidingpuzzle/Conf.java
+++ app/src/main/java/zmuzik/slidingpuzzle/Conf.java
@@ -5,6 +5,9 @@ public class Conf {
     public static final int DEFAULT_GRID_DIM_LONG = 4;
     public static final int DEFAULT_GRID_DIMS_POSITION = 4;
 
+    public static final int PAGE_SIZE_LOCAL = 3;
+    public static final int PAGE_SIZE_FLICKR = 12;
+
     public static final int GRID_COLUMNS_PORTRAIT_HANDHELD = 2;
     public static final int GRID_COLUMNS_LANDSCAPE_HANDHELD = 3;
     public static final int GRID_COLUMNS_PORTRAIT_TABLET = 4;
@@ -12,8 +15,6 @@ public class Conf {
 
     public static final String DCIM_APP_SUBDIR = "SlidingPuzzle";
     public static final int PIC_QUALITY = 90;
-    public static final String PICTURE_FILE_SUFFIX = ".jpg";
-    public static final int MAX_PIC_SIZE = 1000;
 
     public static final String FLICKR_API_ROOT = "https://api.flickr.com/services/rest";
     public static final String FLICKR_API_KEY = "f1434369cbe71e80f75e43f8d374ca39";
